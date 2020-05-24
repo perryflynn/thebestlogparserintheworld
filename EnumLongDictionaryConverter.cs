8f80fb1299e713b524d506454cb89e0132908db6
Christian <christian@serverless.industries>
2020-05-25 00:39:58 +0200

    dynamic enum types for dictionary serialization

--- EnumLongDictionaryConverter.cs
+++ EnumLongDictionaryConverter.cs
@@ -6,17 +6,18 @@ using Newtonsoft.Json.Linq;
 
 namespace logsplit
 {
-    public class EnumLongDictionaryConverter : JsonConverter
+    public class EnumLongDictionaryConverter<TEnum> : JsonConverter
+        where TEnum :  struct, IConvertible
     {
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            var dictionary = value as Dictionary<AddressFamily, long>;
+            var dictionary = value as Dictionary<TEnum, long>;
 
             writer.WriteStartObject();
 
-            foreach (KeyValuePair<AddressFamily, long> pair in dictionary)
+            foreach (KeyValuePair<TEnum, long> pair in dictionary)
             {
-                writer.WritePropertyName(Enum.GetName(typeof(AddressFamily), pair.Key));
+                writer.WritePropertyName(Enum.GetName(typeof(TEnum), pair.Key));
                 writer.WriteValue(pair.Value);
             }
 
@@ -26,11 +27,11 @@ namespace logsplit
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var jObject = JObject.Load(reader);
-            var dict = existingValue as Dictionary<AddressFamily, long>;
+            var dict = existingValue as Dictionary<TEnum, long>;
 
             foreach(var jEl in jObject)
             {
-                AddressFamily keyEnum = (AddressFamily)Enum.Parse(typeof(AddressFamily), jEl.Key);
+                TEnum keyEnum = (TEnum)Enum.Parse(typeof(TEnum), jEl.Key);
                 long value = (long)jEl.Value;
                 dict.Add(keyEnum, value);
             }
@@ -40,7 +41,7 @@ namespace logsplit
 
         public override bool CanConvert(Type objectType)
         {
-            return typeof (IDictionary<AddressFamily, long>) == objectType;
+            return typeof (IDictionary<TEnum, long>) == objectType;
         }
     }
 }
